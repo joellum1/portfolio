bb4548ab55619a3345802de7a27a96e4b82c0716
Joel Lum <joellumzh@gmail.com>
2025-05-06 23:00:00 +1000

    Added styling to about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,7 +1,10 @@
 export default function AboutSection() {
   return (
-    <section id="about">
-
+    <section 
+      id="about"
+      className="flex flex-col items-center pl-4 pr-4 rounded-xl bg-blue-700 text-white"
+    >
+      about
     </section>
   );
 }
