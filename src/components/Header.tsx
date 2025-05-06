357e22205da6aef2c069d5e2b9770993ce79134e
Joel Lum <joellumzh@gmail.com>
2025-05-06 20:51:11 +1000

    Updated Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
 export default function Header() {
   return (
-    <header>
-      header
+    <header className="flex flex-row justify-between items-center">
+      <a href="/" className="text-[1.75rem]">
+        <span className="bold">JOEL LUM</span>
+      </a>
+
+      <nav>
+        <ul className="list-style-none flex flex-row gap-[1.5rem] text-gray-400">
+          <li><a href="/">About Me</a></li>
+          <li><a href="/">Projects</a></li>
+          <li><a href="/">Tech Stack</a></li>
+          <li><a href="/">Extracurriculars</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
