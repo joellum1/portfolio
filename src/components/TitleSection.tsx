ca2cdf4aad6f200feea3d12e53a26b7866d70c2d
Joel Lum <joellumzh@gmail.com>
2025-05-07 00:34:57 +1000

    Added summary sentence to title section

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -4,14 +4,21 @@ export default function TitleSection() {
   return (
     <section 
       id="title"
-      className="flex flex-col items-center rounded-2xl pt-48 pl-24 pr-24"
+      className="flex flex-col items-center rounded-2xl"
     >
-      <div className="flex flex-row items-baseline gap-x-4 text-[6rem]">
+      <div className="flex flex-row items-baseline gap-x-4 text-[6rem] mt-48 mb-48 pl-24 pr-24">
         <h1>Blending</h1>
         <FlipWords
-          words={["logic.", "creativity.", "impact."]}
+          words={["innovation.", "impact.", "creativity."]}
           className="text-blue-700"
         />
+      </div>
+
+      <div className="w-full bg-[#efefef] rounded-2xl p-24 text-[2rem]">
+        <p className="text-[2rem]">
+          👋 Hi, I'm a third-year Software Engineering student at UNSW, passionate about driving{" "}
+          <span className="text-blue-700">change through technology.</span>
+        </p>
       </div>
     </section>
   );
