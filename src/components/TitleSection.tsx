f9d1935a27775600688682b944147dc0cfdae834
Joel Lum <joellumzh@gmail.com>
2025-05-06 22:53:40 +1000

    Updated styling for title section component

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -4,9 +4,9 @@ export default function TitleSection() {
   return (
     <section 
       id="title"
-      className="flex flex-col items-center pt-48 pl-24 pr-24 text-[6rem]"
+      className="flex flex-col items-center pt-48 pl-24 pr-24"
     >
-      <div className="flex flex-row items-baseline gap-x-2">
+      <div className="flex flex-row items-baseline gap-x-4 text-[6rem]">
         <h1>Blending</h1>
         <FlipWords
           words={["logic.", "creativity.", "impact."]}
