cd73e40eb742daac8a4142aedad26eeed62cf0ad
Joel Lum <joellumzh@gmail.com>
2025-05-06 22:34:28 +1000

    Installed dependencies and incorporated flip words

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -1,10 +1,18 @@
+import { FlipWords } from "./ui/flip-words";
+
 export default function TitleSection() {
   return (
     <section 
       id="title"
       className="flex flex-col items-center pt-48 pl-24 pr-24 text-[6rem]"
     >
-      A blend of Logic, Creativity, Impact
+      <div className="flex flex-row items-baseline gap-x-2">
+        <h1>Blending</h1>
+        <FlipWords
+          words={["logic.", "creativity.", "impact."]}
+          className="text-blue-600"
+        />
+      </div>
     </section>
   );
 }
