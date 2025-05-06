94ae0ac6cb9551a78f8d855252beb81c9a45b037
Joel Lum <joellumzh@gmail.com>
2025-05-06 21:52:46 +1000

    Updated landing page to include all sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Header from "@/components/Header"
+import AboutSection from "@/components/AboutSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import TechSection from "@/components/TechSection";
+import ExtraSection from "@/components/ExtraSection";
 
 export default function Home() {
   return (
-    <div>
+    <main className="flex flex-col m-4">
       <Header />
-    </div>
+      <AboutSection />
+      <ProjectsSection />
+      <TechSection />
+      <ExtraSection />
+    </main>
   );
 }
