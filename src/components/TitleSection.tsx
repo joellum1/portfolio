7f3bab2c4f257d751cfd44fe64e8bbb93d0ac162
Joel Lum <joellumzh@gmail.com>
2025-05-08 00:04:09 +1000

    Updated title section summary

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -16,8 +16,8 @@ export default function TitleSection() {
 
       <div className="w-full bg-[#efefef] rounded-2xl p-24 text-[2rem]">
         <p className="text-[2rem]">
-          👋 Hi, I'm a third-year Software Engineering student at UNSW, passionate about driving{" "}
-          <span className="text-blue-700">change through technology.</span>
+          👋 Hi, I'm a third-year Software Engineering student at UNSW, passionate about driving change through technology. 
+          In my strive towards that goal, I'm constantly learning, whether it's through university, personal projects, or collaborating with others to solve real-world problems.
         </p>
       </div>
     </section>
