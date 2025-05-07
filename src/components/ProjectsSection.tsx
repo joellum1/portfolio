e39a021f58de06fa6ea805e934fb7a680f74dc77
Joel Lum <joellumzh@gmail.com>
2025-05-08 01:16:42 +1000

    Starting making projects section

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,7 +1,25 @@
 export default function ProjectsSection() {
-  return (
-    <section id="projects">
+  const projects = [
+    {
+      title: "Portfolio Website",
+      description: "My personal portfolio built with Next.js and Tailwind.",
+      link: "https://yourportfolio.com",
+    },
+    {
+      title: "test",
+      description: "description",
+      link: "https://somelink.com",
+    },
+  ];
 
+  return (
+    <section 
+      id="projects"
+      className="flex flex-col items-center gap-32 bg-[#efefef] rounded-2xl pt-32 pl-16 pr-16"
+    >
+      <h2 className="text-[5rem]">Projects</h2>
+
+      <div></div>
     </section>
   );
 }
